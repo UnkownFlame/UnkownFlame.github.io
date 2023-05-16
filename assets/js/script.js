919617b98b8fea57a32c5a29054f72ed67f2eb8f
UnkownFlame <907106912@qq.com>
2023-05-16 22:03:15 +0800

    add some page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-window.onload = function() {
-    var overlay = document.getElementById("background-overlay");
-    overlay.style.animation = "none";
+function redirectToPage1() {
+    // 使用window.location.href将页面跳转到指定URL
+    window.location.href = "./first.html"; // 将链接替换为您要跳转的URL
   }
+
+function redirectToPage2() {
+    // 使用window.location.href将页面跳转到指定URL
+    window.location.href = "./second.html"; // 将链接替换为您要跳转的URL
+}
+function redirectToPage3() {
+    // 使用window.location.href将页面跳转到指定URL
+    window.location.href = "./about.html"; // 将链接替换为您要跳转的URL
+  }
+
+function redirectToPage4() {
+    // 使用window.location.href将页面跳转到指定URL
+    window.location.href = ""; // 将链接替换为您要跳转的URL
+}
